0b0e4154b647923596d4b55f7b8ad662e523025f
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2021-05-12 13:19:03 +0530

    Spring Boot Logging with Lombok - @Slf4j – Lombok annotations

--- src/main/java/com/ds/logging/SpringBootFeaturesLoggingApplication.java
+++ src/main/java/com/ds/logging/SpringBootFeaturesLoggingApplication.java
@@ -1,13 +1,16 @@
 package com.ds.logging;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootFeaturesLoggingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFeaturesLoggingApplication.class, args);
+		log.info("Simple log statement with inputs {}, {} and {}", 1, 2, 3);
 	}
 
 }
